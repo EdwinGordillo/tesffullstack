93296f0b321951050b8c16770955db5d0feb9277
edwinsillog <edwingordillo802@gmail.com>
2025-06-30 11:23:44 -0500

    Se sube commit con el proyecto inicial generado desde spring initializr

---
+++ backend/src/main/java/co/com/walmart/stefanini/prueba/PruebaWalmart/PruebaWalmartApplication.java
@@ -0,0 +1,12 @@
+package co.com.walmart.stefanini.prueba.PruebaWalmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaWalmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaWalmartApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/co/com/walmart/stefanini/prueba/PruebaWalmart/PruebaWalmartApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.walmart.stefanini.prueba.PruebaWalmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaWalmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
