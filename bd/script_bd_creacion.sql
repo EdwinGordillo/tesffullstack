91571e109aa1dd844bd043efe37cbf8800aee71c
Edwin Leonardo Gordillo Diaz <70527938+EdwinGordillo@users.noreply.github.com>
2025-06-30 14:49:38 -0500

    Update script_bd_creacion.sql
    se ajusta campo para su mejor funcionamiento

--- bd/script_bd_creacion.sql
+++ bd/script_bd_creacion.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS person (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
-    fechaNacimiento DATE NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
     puesto VARCHAR(100) NOT NULL,
     sueldo DECIMAL(10, 2) NOT NULL
 );
